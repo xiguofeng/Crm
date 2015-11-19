a084d135c0a633e729bca7d75573a53d0840621d
xgf <king.xgf@gmail.com>
2015-11-19 11:46:27 +0800

    add:CustomerActivity

---
+++ crm/src/main/java/com/ogg/crm/ui/activity/CustomerActivity.java
@@ -0,0 +1,86 @@
+package com.ogg.crm.ui.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+import com.ogg.crm.R;
+import com.ogg.crm.entity.Menu;
+import com.ogg.crm.ui.adapter.CustomMenuAdapter;
+import com.ogg.crm.ui.view.CustomProgressDialog;
+import com.ogg.crm.ui.view.gridview.CustomGridView;
+import com.ogg.crm.utils.ActivitiyInfoManager;
+
+import java.util.ArrayList;
+
+public class CustomerActivity extends Activity implements OnClickListener {
+
+    private Context mContext;
+
+    private ImageView mBackIv;
+
+    private CustomGridView mCategoryGv;
+    private CustomMenuAdapter mCustomMenuAdapter;
+    private ArrayList<Menu> mMenuList = new ArrayList<Menu>();
+
+
+    private CustomProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer);
+        mContext = CustomerActivity.this;
+        if (!ActivitiyInfoManager.activitityMap
+                .containsKey(ActivitiyInfoManager
+                        .getCurrentActivityName(mContext))) {
+            ActivitiyInfoManager.activitityMap
+                    .put(ActivitiyInfoManager.getCurrentActivityName(mContext),
+                            this);
+        }
+        initView();
+        initData();
+
+    }
+
+    private void initView() {
+        mBackIv = (ImageView) findViewById(R.id.customer_back_iv);
+        mBackIv.setOnClickListener(this);
+
+        mCategoryGv = (CustomGridView) findViewById(R.id.customer_gv);
+        mCustomMenuAdapter = new CustomMenuAdapter(this, mMenuList);
+        mCategoryGv.setAdapter(mCustomMenuAdapter);
+
+    }
+
+    private void initData() {
+        for (int i = 0; i < 2; i++) {
+            Menu menu = new Menu();
+            menu.setLocalImage(R.drawable.ic_launcher);
+            if (i == 0) {
+                menu.setName(getResources().getString(R.string.my_customer));
+            } else {
+                menu.setName(getResources().getString(R.string.public_customer));
+            }
+            mMenuList.add(menu);
+        }
+        mCustomMenuAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.customer_back_iv: {
+
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+    }
+}
